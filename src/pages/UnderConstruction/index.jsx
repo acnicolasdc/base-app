8e4617ffcfdf605e21afb4ac0551e69550c8dac7
acnicolasdc <nicolasreyesbejarano@gmail.com>
2020-11-08 12:26:18 -0500

    ROUTES: not navigation

--- src/pages/UnderConstruction/index.jsx
+++ src/pages/UnderConstruction/index.jsx
@@ -3,22 +3,11 @@ import { View, Text, Button } from "react-native";
 
 const UnderConstruction = ({
   pageName = "Not Found",
-  stack = null,
-  screen = '',
-  navigation,
 }) => {
   const defaultMessage = `Estamos trabajando en aprender ingles ${pageName}`;
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>{defaultMessage}</Text>
-      {screen&&<Button
-        title="GO"
-        onPress={() =>
-          stack
-            ? navigation.navigate(stack, { screen: screen })
-            : navigation.navigate(screen)
-        }
-      />}
     </View>
   );
 };
--- src/routes/public/index.jsx
+++ src/routes/public/index.jsx
@@ -15,11 +15,11 @@ export const routes = Object.freeze({
 const Public = () => {
   return (
       <Stack.Navigator>
-        <Stack.Screen name={routes.ON_BOARDING} component={(props)=><MockPage {...props} pageName={routes.ON_BOARDING} screen={routes.LOGIN}/>} />
-        <Stack.Screen name={routes.LOGIN} component={(props)=><MockPage {...props} pageName={routes.LOGIN} screen={routes.FORGOT_PASSWORD}/>} />
-        <Stack.Screen name={routes.FORGOT_PASSWORD} component={(props)=><MockPage {...props} pageName={routes.FORGOT_PASSWORD} screen={routes.SING_UP}/>} />
-        <Stack.Screen name={routes.SING_UP} component={(props)=><MockPage {...props} pageName={routes.SING_UP} screen={routes.RESET_PASSWORD}/>} />
-        <Stack.Screen name={routes.RESET_PASSWORD} component={(props)=><MockPage {...props} pageName={routes.RESET_PASSWORD} screen={routes.ON_BOARDING}/>} />
+        <Stack.Screen name={routes.ON_BOARDING} component={(props)=><MockPage {...props} pageName={routes.ON_BOARDING}/>} />
+        <Stack.Screen name={routes.LOGIN} component={(props)=><MockPage {...props} pageName={routes.LOGIN} />} />
+        <Stack.Screen name={routes.FORGOT_PASSWORD} component={(props)=><MockPage {...props} pageName={routes.FORGOT_PASSWORD}/>} />
+        <Stack.Screen name={routes.SING_UP} component={(props)=><MockPage {...props} pageName={routes.SING_UP}/>} />
+        <Stack.Screen name={routes.RESET_PASSWORD} component={(props)=><MockPage {...props} pageName={routes.RESET_PASSWORD}/>} />
       </Stack.Navigator>
   );
 };
