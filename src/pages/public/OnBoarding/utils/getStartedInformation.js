5a359305a869402c809defd684e87b4f39d517e1
Nicolas Reyes <36165070+acnicolasdc@users.noreply.github.com>
2021-03-19 00:26:50 -0500

    change texts of OnBoarding (#3)

--- src/pages/public/OnBoarding/utils/getStartedInformation.js
+++ src/pages/public/OnBoarding/utils/getStartedInformation.js
@@ -1,17 +1,30 @@
 export default [
     {
-        title:'Get notified when work happens',
-        text:'Welcome to Envase now you are a envasion.',
+        title:'Crea tu almacén digital',
+        text:'Agrega empleados y define los productos que vendes.',
         image:'image.jpg',
     },
     {
-        title:'Transportation Management System',
-        text:'Envase transforms the transportation ecosystem with a cloud-based.',
+        title:'Identifica tus productos',
+        text:'De forma automática, nuestro sistema le agrega un identificador gráfico único a cada producto',
         image:'image.jpg',
     },
     {
-        title:'Synchronize',
-        text:'Synchronize billions of nodes across the intermodal ecosystem with a single.',
+        title:'Crea ordenes de venta de forma rápida',
+        text:'Tu y tus empleados podran usar la cámara de sus dispositivos para generar las ordenes de venta escaneando los códigos que identifican a cada producto',
+        image:'image.jpg',
+    },
+
+    {
+        title:'Finaliza ventas',
+        text:'Entre mas generas ordenes de venta, mas rápido y mas ventas podrás alcanzar',
+        image:'image.jpg',
+    },
+
+    {
+        title:'Tu negocio crecerá',
+        text:'De la mano, podremos elevar tus ventas de forma novedosa y práctica, intentalo...',
         image:'image.jpg',
     }
+
 ]
