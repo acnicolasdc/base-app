83a47284241c2d7474758deac41886b5ff6d6435
acnicolasdc <nicolasreyesbejarano@gmail.com>
2021-03-22 20:47:33 -0500

    NR: add hook useScrollView as test

---
+++ src/hooks/useScrollView.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+import { Dimensions } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+const useScrollView = () => {
+  const [enable, setEnable] = useState(false);
+  const [height, setHeight] = useState(0);
+  const insets = useSafeAreaInsets();
+  const screenHeight = Math.round(Dimensions.get('window').height);
+  const visibleHeight = screenHeight - insets.bottom - insets.top;
+  useEffect(() => {
+    if (height >= visibleHeight) {
+      setEnable(true);
+    }
+  }, [height, enable]);
+
+  return [enable, setHeight];
+};
+
+export default useScrollView;
