f3958db26403ef739f0c32ecfc58d15830441a73
Nicolas Reyes <nicolasreyesbejarano@gmail.com>
2021-04-25 17:01:28 -0500

    NR: pre login setup

--- src/providers/Authorizations/index.jsx
+++ src/providers/Authorizations/index.jsx
@@ -1,22 +1,67 @@
-import React from "react";
+import React, { useSate } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const APP_NAME = "ORDERWAY";
-
+const USER_KEY = '@user-session-base'
 export const AuthorizationsContext = React.createContext({
+  session: false,
   hasInventoryPermission: () => false,
   getCompanyName: () => APP_NAME,
+  saveUserData: ()=> {},
+  removeUserData: ()=> {}
 });
 
+
+const getData = async ( key="" ) => {
+  try {
+    const value = await AsyncStorage.getItem(key)
+    if(value !== null) {
+      return value;
+    }
+  } catch(e) {
+    return undefined;
+  }
+}
+
 const AuthorizationsProvider = ({ children }) => {
+  const [ session, setSession ] = useSate(false);
+
+  useLayoutEffect(() => {
+    const hasSession = getData(USER_KEY);
+    if (hasSession && !session) {
+        setSession(true);
+    }
+  }, [session]);
+
+
+
   const hasInventoryPermission = () => {
     return true;
   };
 
   const getCompanyName = () => APP_NAME;
 
+  const saveUserData = async (value) => {
+    try {
+      await AsyncStorage.setItem(USER_KEY, value)
+      setSession(true);
+    } catch (e) {
+      // saving error
+    }
+  }
+
+  const removeUserData = async () => {
+    try {
+      await AsyncStorage.removeItem(USER_KEY)
+      setSession(false);
+    } catch (e) {
+      // saving error
+    }
+  }
+
   return (
     <AuthorizationsContext.Provider
-      value={{ hasInventoryPermission, getCompanyName }}
+      value={{ hasInventoryPermission, getCompanyName, saveUserData, removeUserData, session }}
     >
       {children}
     </AuthorizationsContext.Provider>
